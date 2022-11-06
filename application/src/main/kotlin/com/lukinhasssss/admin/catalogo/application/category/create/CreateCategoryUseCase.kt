8675e1d1739e915ef8f4e7e53b94a397579dc9a4
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-11-05 21:28:14 -0300

    feat: add either and notification pattern

--- application/src/main/kotlin/com/lukinhasssss/admin/catalogo/application/category/create/CreateCategoryUseCase.kt
+++ application/src/main/kotlin/com/lukinhasssss/admin/catalogo/application/category/create/CreateCategoryUseCase.kt
@@ -1,5 +1,7 @@
 package com.lukinhasssss.admin.catalogo.application.category.create
 
 import com.lukinhasssss.admin.catalogo.application.UseCase
+import com.lukinhasssss.admin.catalogo.domain.validation.handler.Notification
+import io.vavr.control.Either
 
-abstract class CreateCategoryUseCase : UseCase<CreateCategoryCommand, CreateCategoryOutput>()
+abstract class CreateCategoryUseCase : UseCase<CreateCategoryCommand, Either<Notification, CreateCategoryOutput>>()
--- application/src/main/kotlin/com/lukinhasssss/admin/catalogo/application/category/create/DefaultCreateCategoryUseCase.kt
+++ application/src/main/kotlin/com/lukinhasssss/admin/catalogo/application/category/create/DefaultCreateCategoryUseCase.kt
@@ -2,21 +2,43 @@ package com.lukinhasssss.admin.catalogo.application.category.create
 
 import com.lukinhasssss.admin.catalogo.domain.category.Category
 import com.lukinhasssss.admin.catalogo.domain.category.CategoryGateway
-import com.lukinhasssss.admin.catalogo.domain.validation.handler.ThrowsValidationHandler
+import com.lukinhasssss.admin.catalogo.domain.validation.handler.Notification
+import io.vavr.control.Either
+import io.vavr.kotlin.Try
+import io.vavr.kotlin.left
 
 class DefaultCreateCategoryUseCase(
     private val categoryGateway: CategoryGateway
 ) : CreateCategoryUseCase() {
 
-    override fun execute(aCommand: CreateCategoryCommand): CreateCategoryOutput {
+    override fun execute(aCommand: CreateCategoryCommand): Either<Notification, CreateCategoryOutput> {
         with(aCommand) {
+            val notification = Notification.create()
+
             val aCategory = Category.newCategory(
                 aName = name,
                 aDescription = description,
                 isActive = isActive
-            ).apply { validate(ThrowsValidationHandler()) }
+            ).apply { validate(notification) }
 
-            return CreateCategoryOutput.from(categoryGateway.create(aCategory = aCategory))
+            return if (notification.hasError()) left(notification) else create(aCategory)
         }
     }
+
+    private fun create(aCategory: Category): Either<Notification, CreateCategoryOutput> {
+        return Try { categoryGateway.create(aCategory) }
+            .toEither()
+            .bimap({ Notification.create(it) }, { CreateCategoryOutput.from(it) })
+    }
 }
+
+/*
+    # Outra forma de fazer a funcao create
+
+    private fun create(aCategory: Category): Either<Notification, CreateCategoryOutput> {
+        return Try { categoryGateway.create(aCategory) }
+            .toEither()
+            .mapLeft {Notification.create(it) }
+            .map { CreateCategoryOutput.from(it) }
+    }
+ */
