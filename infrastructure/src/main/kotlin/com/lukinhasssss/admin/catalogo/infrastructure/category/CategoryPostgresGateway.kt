5ccefaf57f267289358a3b5082937a2183707c39
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-11-11 22:03:11 -0300

    feat: create category gateway implementation

---
+++ infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/category/CategoryPostgresGateway.kt
@@ -0,0 +1,34 @@
+package com.lukinhasssss.admin.catalogo.infrastructure.category
+
+import com.lukinhasssss.admin.catalogo.domain.category.Category
+import com.lukinhasssss.admin.catalogo.domain.category.CategoryGateway
+import com.lukinhasssss.admin.catalogo.domain.category.CategoryID
+import com.lukinhasssss.admin.catalogo.domain.category.CategorySearchQuery
+import com.lukinhasssss.admin.catalogo.domain.pagination.Pagination
+import com.lukinhasssss.admin.catalogo.infrastructure.category.persistence.CategoryRepository
+import org.springframework.stereotype.Service
+
+@Service
+class CategoryPostgresGateway(
+    private val categoryRepository: CategoryRepository
+) : CategoryGateway {
+    override fun create(aCategory: Category): Category {
+        TODO("Not yet implemented")
+    }
+
+    override fun findById(anID: CategoryID): Category? {
+        TODO("Not yet implemented")
+    }
+
+    override fun findAll(aQuery: CategorySearchQuery): Pagination<Category> {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(aCategory: Category): Category {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteById(anID: CategoryID) {
+        TODO("Not yet implemented")
+    }
+}
