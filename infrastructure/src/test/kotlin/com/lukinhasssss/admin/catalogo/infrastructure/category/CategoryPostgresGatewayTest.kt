4f3a733cf3f04f69186c92796620389490545e89
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-11-11 22:45:40 -0300

    test: add tests for implementation of method create of category gateway

--- infrastructure/src/test/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/category/CategoryPostgresGatewayTest.kt
+++ infrastructure/src/test/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/category/CategoryPostgresGatewayTest.kt
@@ -1,8 +1,10 @@
 package com.lukinhasssss.admin.catalogo.infrastructure.category
 
+import com.lukinhasssss.admin.catalogo.domain.category.Category
 import com.lukinhasssss.admin.catalogo.infrastructure.PostgresGatewayTest
 import com.lukinhasssss.admin.catalogo.infrastructure.category.persistence.CategoryRepository
-import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 
@@ -16,8 +18,39 @@ class CategoryPostgresGatewayTest {
     private lateinit var categoryRepository: CategoryRepository
 
     @Test
-    fun testInjectedDependencies() {
-        assertNotNull(categoryGateway)
-        assertNotNull(categoryRepository)
+    fun givenAValidCategory_whenCallsCreate_shouldReturnANewCategory() {
+        val expectedName = "Filmes"
+        val expectedDescription = "A categoria mais assistida"
+        val expectedIsActive = true
+
+        val aCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive)
+
+        assertEquals(0, categoryRepository.count())
+
+        val actualCategory = categoryGateway.create(aCategory)
+
+        assertEquals(1, categoryRepository.count())
+
+        with(actualCategory) {
+            assertEquals(aCategory.id.value, id.value)
+            assertEquals(expectedName, name)
+            assertEquals(expectedDescription, description)
+            assertEquals(expectedIsActive, isActive)
+            assertEquals(aCategory.createdAt, createdAt)
+            assertEquals(aCategory.updatedAt, updatedAt)
+            assertNull(deletedAt)
+        }
+
+        val actualEntity = categoryRepository.findById(aCategory.id.value).get()
+
+        with(actualEntity) {
+            assertEquals(aCategory.id.value, id)
+            assertEquals(expectedName, name)
+            assertEquals(expectedDescription, description)
+            assertEquals(expectedIsActive, isActive)
+            assertEquals(aCategory.createdAt, createdAt)
+            assertEquals(aCategory.updatedAt, updatedAt)
+            assertNull(deletedAt)
+        }
     }
 }
