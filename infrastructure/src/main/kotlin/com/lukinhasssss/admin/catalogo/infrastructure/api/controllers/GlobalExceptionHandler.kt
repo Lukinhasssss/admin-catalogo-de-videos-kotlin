2b6e5722566a7f72445c10ac39e32cc6db8ff74f
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-11-13 10:27:30 -0300

    feat: implement global exception handler

---
+++ infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/api/controllers/GlobalExceptionHandler.kt
@@ -0,0 +1,24 @@
+package com.lukinhasssss.admin.catalogo.infrastructure.api.controllers
+
+import com.lukinhasssss.admin.catalogo.domain.exception.DomainException
+import com.lukinhasssss.admin.catalogo.domain.validation.Error
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = [DomainException::class])
+    fun handleDomainException(ex: DomainException): ResponseEntity<Any> {
+        return ResponseEntity.unprocessableEntity().body(ApiError.from(ex))
+    }
+
+    data class ApiError(val message: String?, val errors: List<Error>) {
+        companion object {
+            fun from(ex: DomainException) = with(ex) {
+                ApiError(message, errors)
+            }
+        }
+    }
+}
