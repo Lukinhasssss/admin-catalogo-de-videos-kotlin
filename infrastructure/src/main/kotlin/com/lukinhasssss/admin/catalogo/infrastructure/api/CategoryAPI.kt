e41ac218b816e9f67d5f92b1a3510680c286ffa0
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-11-12 23:02:51 -0300

    feat: add springdoc-openapi annotations

--- infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/api/CategoryAPI.kt
+++ infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/api/CategoryAPI.kt
@@ -1,6 +1,10 @@
 package com.lukinhasssss.admin.catalogo.infrastructure.api
 
 import com.lukinhasssss.admin.catalogo.domain.pagination.Pagination
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -9,12 +13,29 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 
 @RequestMapping(value = ["categories"])
+@Tag(name = "Categories")
 interface CategoryAPI {
 
     @PostMapping(consumes = [APPLICATION_JSON_VALUE], produces = [APPLICATION_JSON_VALUE])
+    @Operation(summary = "Create a new category")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "201", description = "Created successfully"),
+            ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+            ApiResponse(responseCode = "500", description = "An internal server error was thrown")
+        ]
+    )
     fun createCategory(): ResponseEntity<*>
 
-    @GetMapping
+    @GetMapping(produces = [APPLICATION_JSON_VALUE])
+    @Operation(summary = "Create a new category")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Listed successfully"),
+            ApiResponse(responseCode = "422", description = "An invalid parameter was received"),
+            ApiResponse(responseCode = "500", description = "An internal server error was thrown")
+        ]
+    )
     fun listCategories(
         @RequestParam(name = "search", required = false, defaultValue = "") search: String,
         @RequestParam(name = "page", required = false, defaultValue = "0") page: Int,
