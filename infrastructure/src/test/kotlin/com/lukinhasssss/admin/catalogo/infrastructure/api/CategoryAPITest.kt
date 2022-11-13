35b04b671b5b1d9875211f99dab0f44b55fe5932
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-11-13 10:28:05 -0300

    test: add more tests for implementation of create category endpoint

--- infrastructure/src/test/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/api/CategoryAPITest.kt
+++ infrastructure/src/test/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/api/CategoryAPITest.kt
@@ -4,9 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.lukinhasssss.admin.catalogo.ControllerTest
 import com.lukinhasssss.admin.catalogo.application.category.create.CreateCategoryOutput
 import com.lukinhasssss.admin.catalogo.application.category.create.CreateCategoryUseCase
+import com.lukinhasssss.admin.catalogo.domain.exception.DomainException
+import com.lukinhasssss.admin.catalogo.domain.validation.Error
+import com.lukinhasssss.admin.catalogo.domain.validation.handler.Notification
 import com.lukinhasssss.admin.catalogo.infrastructure.category.models.CreateCategoryRequest
+import io.vavr.kotlin.left
 import io.vavr.kotlin.right
 import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.nullValue
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
 import org.mockito.kotlin.argThat
@@ -60,12 +65,87 @@ class CategoryAPITest {
         val response = mvc.perform(request).andDo(print())
 
         // then
-        response.andExpect {
-            status().isCreated
-            header().string("Location", "/categories/123")
-            header().string("Content-Type", APPLICATION_JSON_VALUE)
-            jsonPath("$.id", equalTo("123"))
-        }
+        response.andExpect(status().isCreated)
+            .andExpect(header().string("Location", "/categories/123"))
+            .andExpect(header().string("Content-Type", APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.id", equalTo("123")))
+
+        verify(createCategoryUseCase, times(1)).execute(
+            argThat {
+                expectedName == name && expectedDescription == description && expectedIsActive == isActive
+            }
+        )
+    }
+
+    @Test
+    fun givenAnInvalidName_whenCallsCreateCategory_shouldReturnNotification() {
+        // given
+        val expectedName = "      "
+        val expectedDescription = "A categoria mais assistida"
+        val expectedIsActive = true
+        val expectedMessage = "'name' should not be empty"
+
+        val anInput = CreateCategoryRequest(
+            name = expectedName,
+            description = expectedDescription,
+            active = expectedIsActive
+        )
+
+        whenever(createCategoryUseCase.execute(any()))
+            .thenReturn(left(Notification.create(Error(expectedMessage))))
+
+        // when
+        val request = post("/categories")
+            .contentType(APPLICATION_JSON)
+            .content(mapper.writeValueAsString(anInput))
+
+        val response = mvc.perform(request).andDo(print())
+
+        // then
+        response.andExpect(status().isUnprocessableEntity)
+            .andExpect(header().string("Location", nullValue()))
+            .andExpect(header().string("Content-Type", APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.errors.size()", equalTo(1)))
+            .andExpect(jsonPath("$.errors[0].message", equalTo(expectedMessage)))
+
+        verify(createCategoryUseCase, times(1)).execute(
+            argThat {
+                expectedName == name && expectedDescription == description && expectedIsActive == isActive
+            }
+        )
+    }
+
+    @Test
+    fun givenAnInvalidCommand_whenCallsCreateCategory_shouldReturnDomainException() {
+        // given
+        val expectedName = "      "
+        val expectedDescription = "A categoria mais assistida"
+        val expectedIsActive = true
+        val expectedMessage = "'name' should not be empty"
+
+        val anInput = CreateCategoryRequest(
+            name = expectedName,
+            description = expectedDescription,
+            active = expectedIsActive
+        )
+
+        whenever(createCategoryUseCase.execute(any()))
+            .thenThrow(DomainException.with(Error(expectedMessage)))
+
+        // when
+        val request = post("/categories")
+            .contentType(APPLICATION_JSON)
+            .content(mapper.writeValueAsString(anInput))
+
+        val response = mvc.perform(request).andDo(print())
+
+        // then
+        response.andExpect(status().isUnprocessableEntity)
+            .andExpect(header().string("Location", nullValue()))
+            .andExpect(header().string("Content-Type", APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.message", equalTo(expectedMessage)))
+            .andExpect(jsonPath("$.errors.size()", equalTo(1)))
+            .andExpect(jsonPath("$.errors[0].message", equalTo(expectedMessage)))
 
         verify(createCategoryUseCase, times(1)).execute(
             argThat {
