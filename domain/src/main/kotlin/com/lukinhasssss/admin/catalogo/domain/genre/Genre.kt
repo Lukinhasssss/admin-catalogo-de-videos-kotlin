f042623ceecf81b81ead71ce894510a212db8da7
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-11-15 16:11:12 -0300

    feat: add genre aggregate

---
+++ domain/src/main/kotlin/com/lukinhasssss/admin/catalogo/domain/genre/Genre.kt
@@ -0,0 +1,47 @@
+package com.lukinhasssss.admin.catalogo.domain.genre
+
+import com.lukinhasssss.admin.catalogo.domain.AggregateRoot
+import com.lukinhasssss.admin.catalogo.domain.category.CategoryID
+import com.lukinhasssss.admin.catalogo.domain.validation.ValidationHandler
+import java.time.Instant
+
+class Genre(
+    override val id: GenreID,
+    val name: String,
+    val active: Boolean,
+    val categories: List<CategoryID>,
+    val createdAt: Instant,
+    val updatedAt: Instant,
+    val deletedAt: Instant?
+) : AggregateRoot<GenreID>(id) {
+
+    companion object {
+        fun newGenre(aName: String, isActive: Boolean): Genre {
+            val anId = GenreID.unique()
+            val now = Instant.now()
+            val deletedAt = if (isActive) null else now
+
+            return Genre(anId, aName, isActive, mutableListOf(), now, now, deletedAt)
+        }
+
+        fun with(
+            anId: GenreID,
+            aName: String,
+            isActive: Boolean,
+            categories: List<CategoryID>,
+            createdAt: Instant,
+            updatedAt: Instant,
+            deletedAt: Instant?
+        ) = Genre(anId, aName, isActive, categories, createdAt, updatedAt, deletedAt)
+
+        fun with(aGenre: Genre) = with(aGenre) {
+            Genre(id, name, active, categories, createdAt, updatedAt, deletedAt)
+        }
+    }
+
+    fun isActive() = active
+
+    override fun validate(handler: ValidationHandler) {
+        TODO("Not yet implemented")
+    }
+}
