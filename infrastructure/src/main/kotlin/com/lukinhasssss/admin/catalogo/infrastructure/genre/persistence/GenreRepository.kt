83f2de567da7ae58ce80a7a4f1fa3257c1e97f02
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-12-04 11:27:09 -0300

    feat: implement find all method of genre gateway

--- infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/genre/persistence/GenreRepository.kt
+++ infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/genre/persistence/GenreRepository.kt
@@ -1,5 +1,14 @@
 package com.lukinhasssss.admin.catalogo.infrastructure.genre.persistence
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.domain.Specification
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface GenreRepository : JpaRepository<GenreJpaEntity, String>
+interface GenreRepository : JpaRepository<GenreJpaEntity, String> {
+
+    fun findAll(
+        whereClause: Specification<GenreJpaEntity>,
+        page: Pageable
+    ): Page<GenreJpaEntity>
+}
