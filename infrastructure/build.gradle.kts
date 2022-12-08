2eeb62cd8327aa3122663cfc1c1750c3415a5d72
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-12-08 18:35:24 -0300

    build: update dependencies

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     id("io.gitlab.arturbosch.detekt") version "1.22.0-RC2"
     id("org.springframework.boot") version "2.7.5"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
-    id("org.flywaydb.flyway") version "9.8.2"
+    id("org.flywaydb.flyway") version "9.8.3"
 }
 
 group = "com.lukinhasssss.admin.catalogo.infrastructure"
@@ -54,10 +54,10 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.h2database:h2")
-    testImplementation("org.flywaydb:flyway-core:9.8.2")
+    testImplementation("org.flywaydb:flyway-core:9.8.3")
 
-    testImplementation("com.ninja-squad:springmockk:3.1.1")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
+    testImplementation("com.ninja-squad:springmockk:3.1.2")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
     testImplementation("io.rest-assured:kotlin-extensions:5.3.0")
 
     testImplementation("org.testcontainers:testcontainers:1.17.6")
