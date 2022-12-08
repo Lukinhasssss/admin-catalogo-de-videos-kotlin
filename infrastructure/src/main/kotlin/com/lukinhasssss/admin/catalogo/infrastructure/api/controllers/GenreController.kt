67ec31191d1b18286ba24d629345f4054c3459f1
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-12-07 22:15:16 -0300

    feat: implement get genre by id endpoint

--- infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/api/controllers/GenreController.kt
+++ infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/api/controllers/GenreController.kt
@@ -2,19 +2,21 @@ package com.lukinhasssss.admin.catalogo.infrastructure.api.controllers
 
 import com.lukinhasssss.admin.catalogo.application.genre.create.CreateGenreCommand
 import com.lukinhasssss.admin.catalogo.application.genre.create.CreateGenreUseCase
+import com.lukinhasssss.admin.catalogo.application.genre.retrive.get.GetGenreByIdUseCase
 import com.lukinhasssss.admin.catalogo.domain.pagination.Pagination
 import com.lukinhasssss.admin.catalogo.infrastructure.api.GenreAPI
 import com.lukinhasssss.admin.catalogo.infrastructure.genre.models.CreateGenreRequest
 import com.lukinhasssss.admin.catalogo.infrastructure.genre.models.GenreListResponse
-import com.lukinhasssss.admin.catalogo.infrastructure.genre.models.GenreResponse
 import com.lukinhasssss.admin.catalogo.infrastructure.genre.models.UpdateGenreRequest
+import com.lukinhasssss.admin.catalogo.infrastructure.genre.presenters.toGenreResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 import java.net.URI
 
 @RestController
 class GenreController(
-    val createGenreUseCase: CreateGenreUseCase
+    val createGenreUseCase: CreateGenreUseCase,
+    val getGenreByIdUseCase: GetGenreByIdUseCase
 ) : GenreAPI {
 
     override fun create(request: CreateGenreRequest): ResponseEntity<Any> = with(request) {
@@ -35,9 +37,7 @@ class GenreController(
         TODO("Not yet implemented")
     }
 
-    override fun getById(id: String): GenreResponse {
-        TODO("Not yet implemented")
-    }
+    override fun getById(id: String) = getGenreByIdUseCase.execute(id).toGenreResponse()
 
     override fun updateById(id: String, request: UpdateGenreRequest): ResponseEntity<Any> {
         TODO("Not yet implemented")
