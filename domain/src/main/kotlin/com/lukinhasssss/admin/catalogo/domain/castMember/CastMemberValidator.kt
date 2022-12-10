e71e711906f32035c8e5e8cb1195c33fc041b327
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-12-10 19:57:40 -0300

    feat: implement cast member validator

--- domain/src/main/kotlin/com/lukinhasssss/admin/catalogo/domain/castMember/CastMemberValidator.kt
+++ domain/src/main/kotlin/com/lukinhasssss/admin/catalogo/domain/castMember/CastMemberValidator.kt
@@ -1,5 +1,6 @@
 package com.lukinhasssss.admin.catalogo.domain.castMember
 
+import com.lukinhasssss.admin.catalogo.domain.validation.Error
 import com.lukinhasssss.admin.catalogo.domain.validation.ValidationHandler
 import com.lukinhasssss.admin.catalogo.domain.validation.Validator
 
@@ -8,5 +9,24 @@ class CastMemberValidator(
     validationHandler: ValidationHandler
 ) : Validator(validationHandler) {
 
-    override fun validate() {}
+    override fun validate() = checkNameConstraints()
+
+    companion object {
+        private const val MIN_NAME_LENGTH = 3
+        private const val MAX_NAME_LENGTH = 255
+    }
+
+    private fun checkNameConstraints() {
+        with(castMember.name) {
+            if (isBlank()) {
+                validationHandler.append(Error(message = "'name' should not be empty"))
+                return
+            }
+
+            val nameLength = trim().length
+
+            if (nameLength < MIN_NAME_LENGTH || nameLength > MAX_NAME_LENGTH)
+                validationHandler.append(Error(message = "'name' must be between 3 and 255 characters"))
+        }
+    }
 }
