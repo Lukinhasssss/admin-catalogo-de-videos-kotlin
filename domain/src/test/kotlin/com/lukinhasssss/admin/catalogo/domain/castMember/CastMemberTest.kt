0e9e8b292e81aa0257a55467b4d7ab0303f8d683
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-12-10 19:58:21 -0300

    test: add tests for cast member validator

--- domain/src/test/kotlin/com/lukinhasssss/admin/catalogo/domain/castMember/CastMemberTest.kt
+++ domain/src/test/kotlin/com/lukinhasssss/admin/catalogo/domain/castMember/CastMemberTest.kt
@@ -1,6 +1,8 @@
 package com.lukinhasssss.admin.catalogo.domain.castMember
 
+import com.lukinhasssss.admin.catalogo.domain.exception.NotificationException
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
@@ -22,5 +24,53 @@ class CastMemberTest {
             assertNotNull(updatedAt)
             assertEquals(createdAt, updatedAt)
         }
+    }
+
+    @Test
+    fun givenAnInvalidEmptyName_whenCallsNewMember_shouldReceiveANotification() {
+        val expectedName = "   "
+        val expectedType = CastMemberType.ACTOR
+        val expectedErrorCount = 1
+        val expectedErrorMessage = "'name' should not be empty"
+
+        val actualException = assertThrows<NotificationException> {
+            CastMember.newMember(expectedName, expectedType)
+        }
+
+        assertNotNull(actualException)
+        assertEquals(expectedErrorCount, actualException.errors.size)
+        assertEquals(expectedErrorMessage, actualException.errors[0].message)
+    }
+
+    @Test
+    fun givenAnInvalidNameWithLengthLessThan3_whenCallsNewMember_shouldReceiveANotification() {
+        val expectedName = "ab       "
+        val expectedType = CastMemberType.ACTOR
+        val expectedErrorCount = 1
+        val expectedErrorMessage = "'name' must be between 3 and 255 characters"
+
+        val actualException = assertThrows<NotificationException> {
+            CastMember.newMember(expectedName, expectedType)
+        }
+
+        assertNotNull(actualException)
+        assertEquals(expectedErrorCount, actualException.errors.size)
+        assertEquals(expectedErrorMessage, actualException.errors[0].message)
+    }
+
+    @Test
+    fun givenAnInvalidNameWithLengthMoreThan255_whenCallsNewMember_shouldReceiveANotification() {
+        val expectedName = "a".repeat(256)
+        val expectedType = CastMemberType.ACTOR
+        val expectedErrorCount = 1
+        val expectedErrorMessage = "'name' must be between 3 and 255 characters"
+
+        val actualException = assertThrows<NotificationException> {
+            CastMember.newMember(expectedName, expectedType)
+        }
+
+        assertNotNull(actualException)
+        assertEquals(expectedErrorCount, actualException.errors.size)
+        assertEquals(expectedErrorMessage, actualException.errors[0].message)
     }
 }
