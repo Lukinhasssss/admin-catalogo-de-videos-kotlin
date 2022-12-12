98e17b24eafbd24e1409c2412d3ed1e81afa472d
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-12-11 21:43:38 -0300

    test: add test for implementation of method update of cast member gateway

--- infrastructure/src/test/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/castMember/CastMemberPostgresGatewayTest.kt
+++ infrastructure/src/test/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/castMember/CastMemberPostgresGatewayTest.kt
@@ -4,9 +4,12 @@ import com.lukinhasssss.admin.catalogo.Fixture
 import com.lukinhasssss.admin.catalogo.PostgresGatewayTest
 import com.lukinhasssss.admin.catalogo.domain.castMember.CastMember
 import com.lukinhasssss.admin.catalogo.domain.castMember.CastMemberGateway
+import com.lukinhasssss.admin.catalogo.domain.castMember.CastMemberType
+import com.lukinhasssss.admin.catalogo.infrastructure.castMember.persistence.CastMemberJpaEntity
 import com.lukinhasssss.admin.catalogo.infrastructure.castMember.persistence.CastMemberRepository
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 
@@ -59,6 +62,47 @@ class CastMemberPostgresGatewayTest {
             assertEquals(expectedType, type)
             assertEquals(aMember.createdAt, createdAt)
             assertEquals(aMember.updatedAt, updatedAt)
+        }
+    }
+
+    @Test
+    fun givenAValidCastMember_whenCallsUpdate_shouldRefreshIt() {
+        // given
+        val expectedName = Fixture.name()
+        val expectedType = CastMemberType.ACTOR
+
+        val aMember = CastMember.newMember("A Pedra", CastMemberType.DIRECTOR)
+
+        val expectedId = aMember.id
+
+        castMemberRepository.saveAndFlush(CastMemberJpaEntity.from(aMember))
+
+        assertEquals(1, castMemberRepository.count())
+
+        // when
+        val actualMember = castMemberGateway.update(
+            aMember.update(expectedName, expectedType)
+        )
+
+        // then
+        assertEquals(1, castMemberRepository.count())
+
+        with(actualMember) {
+            assertEquals(expectedId, id)
+            assertEquals(expectedName, name)
+            assertEquals(expectedType, type)
+            assertEquals(aMember.createdAt, createdAt)
+            assertTrue(aMember.updatedAt.isBefore(updatedAt))
+        }
+
+        val persistedMember = castMemberRepository.findById(expectedId.value).get()
+
+        with(persistedMember) {
+            assertEquals(expectedId.value, id)
+            assertEquals(expectedName, name)
+            assertEquals(expectedType, type)
+            assertEquals(aMember.createdAt, createdAt)
+            assertTrue(aMember.updatedAt.isBefore(updatedAt))
         }
     }
 }
