87c35f46d012e2526997b732c8ffae34dcf62af1
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-12-11 22:25:13 -0300

    feat: implement method find all of cast member gateway

--- infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/castMember/CastMemberPostgresGateway.kt
+++ infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/castMember/CastMemberPostgresGateway.kt
@@ -7,7 +7,12 @@ import com.lukinhasssss.admin.catalogo.domain.pagination.Pagination
 import com.lukinhasssss.admin.catalogo.domain.pagination.SearchQuery
 import com.lukinhasssss.admin.catalogo.infrastructure.castMember.persistence.CastMemberJpaEntity
 import com.lukinhasssss.admin.catalogo.infrastructure.castMember.persistence.CastMemberRepository
+import com.lukinhasssss.admin.catalogo.infrastructure.utils.SpecificationUtils
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
+import org.springframework.data.jpa.domain.Specification
 import org.springframework.stereotype.Component
+import java.util.Optional
 
 @Component
 class CastMemberPostgresGateway(
@@ -19,8 +24,20 @@ class CastMemberPostgresGateway(
     override fun findById(anID: CastMemberID): CastMember? =
         repository.findById(anID.value).map { it.toAggregate() }.orElse(null)
 
-    override fun findAll(aQuery: SearchQuery): Pagination<CastMember> {
-        TODO("Not yet implemented")
+    override fun findAll(aQuery: SearchQuery): Pagination<CastMember> = with(aQuery) {
+        // Paginação
+        val page = PageRequest.of(page, perPage, Sort.by(Sort.Direction.fromString(direction), sort))
+
+        // Busca dinâmica pelo critério terms (name ou description)
+        val specifications = Optional.ofNullable(terms)
+            .filter { it.isNotBlank() }
+            .map { assembleSpecification(it) }.orElse(null)
+
+        val pageResult = repository.findAll(specification = Specification.where(specifications), page = page)
+
+        with(pageResult) {
+            Pagination(number, size, totalElements, map { it.toAggregate() }.toList())
+        }
     }
 
     override fun update(aCastMember: CastMember): CastMember = save(aCastMember)
@@ -28,6 +45,8 @@ class CastMemberPostgresGateway(
     override fun deleteById(anID: CastMemberID) = with(anID.value) {
         if (repository.existsById(this)) repository.deleteById(this)
     }
+
+    private fun assembleSpecification(term: String) = SpecificationUtils.like<CastMemberJpaEntity>("name", term)
 
     private fun save(aMember: CastMember) = repository.save(CastMemberJpaEntity.from(aMember)).toAggregate()
 }
