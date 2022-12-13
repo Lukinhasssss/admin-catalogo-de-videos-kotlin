9ab7e570b84cae58f44f2a997c8cbd571aa96536
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-12-13 10:23:01 -0300

    test: add serialization test for CastMemberResponse

---
+++ infrastructure/src/test/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/castMember/models/CastMemberResponseTest.kt
@@ -0,0 +1,44 @@
+package com.lukinhasssss.admin.catalogo.infrastructure.castMember.models
+
+import com.lukinhasssss.admin.catalogo.Fixture
+import com.lukinhasssss.admin.catalogo.JacksonTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.json.JacksonTester
+import java.time.Instant
+
+@JacksonTest
+class CastMemberResponseTest {
+
+    @Autowired
+    private lateinit var json: JacksonTester<CastMemberResponse>
+
+    @Test
+    fun testMarshall() {
+        val expectId = "123"
+        val expectedName = Fixture.name()
+        val expectedType = Fixture.CastMember.type().name
+        val expectedCreatedAt = Instant.now()
+        val expectedUpdatedAt = Instant.now()
+
+        val response = CastMemberResponse(
+            id = expectId,
+            name = expectedName,
+            type = expectedType,
+            createdAt = expectedCreatedAt.toString(),
+            updatedAt = expectedUpdatedAt.toString()
+        )
+
+        val actualJson = json.write(response)
+
+        assertThat(actualJson)
+            .hasJsonPathValue("$.id", expectId)
+            .hasJsonPathValue("$.name", expectedName)
+            .hasJsonPathValue("$.type", expectedType)
+            .hasJsonPathValue("$.created_at", expectedCreatedAt)
+            .hasJsonPathValue("$.updated_at", expectedUpdatedAt)
+    }
+}
+
+// Marshall (Serializacao) -> Conversao de um objeto para json
