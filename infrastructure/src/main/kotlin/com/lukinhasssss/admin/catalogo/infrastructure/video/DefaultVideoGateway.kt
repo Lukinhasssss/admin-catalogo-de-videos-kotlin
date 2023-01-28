06aab9193bbc3fec6630f0d1b1ceac42aafbc78e
Lucas Monteiro <lukasmonteir0@hotmail.com>
2023-01-28 09:57:29 -0300

    feat: implement method create of video gateway

---
+++ infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/video/DefaultVideoGateway.kt
@@ -0,0 +1,36 @@
+package com.lukinhasssss.admin.catalogo.infrastructure.video
+
+import com.lukinhasssss.admin.catalogo.domain.pagination.Pagination
+import com.lukinhasssss.admin.catalogo.domain.video.Video
+import com.lukinhasssss.admin.catalogo.domain.video.VideoGateway
+import com.lukinhasssss.admin.catalogo.domain.video.VideoID
+import com.lukinhasssss.admin.catalogo.domain.video.VideoSearchQuery
+import com.lukinhasssss.admin.catalogo.infrastructure.video.persistence.VideoJpaEntity
+import com.lukinhasssss.admin.catalogo.infrastructure.video.persistence.VideoRepository
+import jakarta.transaction.Transactional
+
+class DefaultVideoGateway(
+    private val videoRepository: VideoRepository
+) : VideoGateway {
+
+    @Transactional
+    override fun create(aVideo: Video): Video {
+        return videoRepository.save(VideoJpaEntity.from(aVideo)).toAggregate()
+    }
+
+    override fun findById(anID: VideoID): Video? {
+        TODO("Not yet implemented")
+    }
+
+    override fun findAll(aQuery: VideoSearchQuery): Pagination<Video> {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(aVideo: Video): Video {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteById(anID: VideoID) {
+        TODO("Not yet implemented")
+    }
+}
