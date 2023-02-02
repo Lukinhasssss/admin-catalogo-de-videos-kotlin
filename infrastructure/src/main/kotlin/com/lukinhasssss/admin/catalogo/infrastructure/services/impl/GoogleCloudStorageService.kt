ce44319b0efc2089f99ac7f4d1e0c2bdd9abe0f3
Lucas Monteiro <lukasmonteir0@hotmail.com>
2023-02-02 10:11:20 -0300

    feat: create facade for storage service

---
+++ infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/services/impl/GoogleCloudStorageService.kt
@@ -0,0 +1,51 @@
+package com.lukinhasssss.admin.catalogo.infrastructure.services.impl
+
+import com.google.cloud.storage.Blob
+import com.google.cloud.storage.BlobId
+import com.google.cloud.storage.BlobInfo
+import com.google.cloud.storage.Storage
+import com.lukinhasssss.admin.catalogo.domain.video.Resource
+import com.lukinhasssss.admin.catalogo.infrastructure.services.StorageService
+import java.util.stream.StreamSupport
+
+class GoogleCloudStorageService(
+    private val bucket: String,
+    private val storage: Storage
+) : StorageService {
+
+    override fun store(name: String, resource: Resource) {
+        val blobInfo = BlobInfo.newBuilder(bucket, name)
+            .setContentType(resource.contentType)
+            .setCrc32cFromHexString("")
+            .build()
+
+        storage.create(blobInfo, resource.content)
+    }
+
+    override fun list(prefix: String): List<String> {
+        val blobs = storage.list(bucket, Storage.BlobListOption.prefix(prefix))
+
+        return StreamSupport.stream(blobs.iterateAll().spliterator(), false)
+            .map { it.blobId }
+            .map { it.name }
+            .toList()
+    }
+
+    override fun deleteAll(names: Collection<String>) {
+        val blobs = names.map { BlobId.of(bucket, it) }
+
+        storage.delete(blobs)
+    }
+
+    override fun get(name: String): Resource? =
+        storage.get(bucket, name).toResource()
+
+    private fun Blob?.toResource() = if (this != null) {
+        Resource.with(
+            content = getContent(),
+            contentType = contentType,
+            name = name,
+            type = Resource.Type.VIDEO // TODO: Ver se sera necesario alterar para poder receber nulo
+        )
+    } else null
+}
---
+++ infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/services/local/InMemoryStorageService.kt
@@ -0,0 +1,26 @@
+package com.lukinhasssss.admin.catalogo.infrastructure.services.local
+
+import com.lukinhasssss.admin.catalogo.domain.video.Resource
+import com.lukinhasssss.admin.catalogo.infrastructure.services.StorageService
+
+class InMemoryStorageService(
+    private val storage: HashMap<String, Resource> = hashMapOf()
+) : StorageService {
+
+    override fun store(name: String, resource: Resource) {
+        storage[name] = resource
+    }
+
+    override fun list(prefix: String): List<String> =
+        storage.keys.filter { it.startsWith(prefix) }
+
+    override fun deleteAll(names: Collection<String>) =
+        names.forEach(storage::remove)
+
+    override fun get(name: String): Resource? =
+        storage[name]
+
+    fun storage(): Map<String, Resource> = storage
+
+    fun reset() = storage.clear()
+}
