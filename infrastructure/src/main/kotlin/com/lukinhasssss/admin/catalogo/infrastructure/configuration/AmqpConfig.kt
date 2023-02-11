d8b14091231341bcef9c0ab2b8f62eaf38cb6b47
Lucas Monteiro <lukasmonteir0@hotmail.com>
2023-02-11 20:14:43 -0300

    feat: automate rabbitmq setup with amqp admin

--- infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/configuration/AmqpConfig.kt
+++ infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/configuration/AmqpConfig.kt
@@ -2,7 +2,12 @@ package com.lukinhasssss.admin.catalogo.infrastructure.configuration
 
 import com.lukinhasssss.admin.catalogo.infrastructure.configuration.annotations.VideoCreatedQueue
 import com.lukinhasssss.admin.catalogo.infrastructure.configuration.annotations.VideoEncodedQueue
+import com.lukinhasssss.admin.catalogo.infrastructure.configuration.annotations.VideoEvents
 import com.lukinhasssss.admin.catalogo.infrastructure.configuration.properties.amqp.QueueProperties
+import org.springframework.amqp.core.Binding
+import org.springframework.amqp.core.BindingBuilder
+import org.springframework.amqp.core.DirectExchange
+import org.springframework.amqp.core.Queue
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -21,7 +26,37 @@ class AmqpConfig {
     fun videoEncodedQueueProperties() = QueueProperties()
 
     @Bean
-    fun queueName(
-        @VideoCreatedQueue props: QueueProperties
-    ) = props.queue
+    fun queueName(@VideoCreatedQueue props: QueueProperties) = props.queue
+
+    @Configuration
+    class Admin {
+
+        @Bean
+        @VideoEvents
+        fun videoEventsExchange(@VideoCreatedQueue props: QueueProperties) = DirectExchange(props.exchange)
+
+        @Bean
+        @VideoCreatedQueue
+        fun videoCreatedQueue(@VideoCreatedQueue props: QueueProperties) = Queue(props.queue)
+
+        @Bean
+        @VideoCreatedQueue
+        fun videoCreatedBinding(
+            @VideoEvents exchange: DirectExchange,
+            @VideoCreatedQueue queue: Queue,
+            @VideoCreatedQueue props: QueueProperties
+        ): Binding = BindingBuilder.bind(queue).to(exchange).with(props.routingKey)
+
+        @Bean
+        @VideoEncodedQueue
+        fun videoEncodedQueue(@VideoEncodedQueue props: QueueProperties) = Queue(props.queue)
+
+        @Bean
+        @VideoEncodedQueue
+        fun videoEncodedBinding(
+            @VideoEvents exchange: DirectExchange,
+            @VideoEncodedQueue queue: Queue,
+            @VideoEncodedQueue props: QueueProperties
+        ): Binding = BindingBuilder.bind(queue).to(exchange).with(props.routingKey)
+    }
 }
