3c089a8b8a68b45fdc77e9a349f9bd64f6122ab6
Lucas Monteiro <lukasmonteir0@hotmail.com>
2023-02-11 18:30:44 -0300

    feat: qualify beans for spring injection

--- infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/configuration/AmqpConfig.kt
+++ infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/configuration/AmqpConfig.kt
@@ -1,5 +1,7 @@
 package com.lukinhasssss.admin.catalogo.infrastructure.configuration
 
+import com.lukinhasssss.admin.catalogo.infrastructure.configuration.annotations.VideoCreatedQueue
+import com.lukinhasssss.admin.catalogo.infrastructure.configuration.annotations.VideoEncodedQueue
 import com.lukinhasssss.admin.catalogo.infrastructure.configuration.properties.amqp.QueueProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
@@ -9,10 +11,17 @@ import org.springframework.context.annotation.Configuration
 class AmqpConfig {
 
     @Bean
+    @VideoCreatedQueue
     @ConfigurationProperties(value = "amqp.queues.video-created")
     fun videoCreatedQueueProperties() = QueueProperties()
 
     @Bean
+    @VideoEncodedQueue
     @ConfigurationProperties(value = "amqp.queues.video-encoded")
     fun videoEncodedQueueProperties() = QueueProperties()
+
+    @Bean
+    fun queueName(
+        @VideoCreatedQueue props: QueueProperties
+    ) = props.queue
 }
---
+++ infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/configuration/annotations/VideoEvents.kt
@@ -0,0 +1,13 @@
+package com.lukinhasssss.admin.catalogo.infrastructure.configuration.annotations
+
+import org.springframework.beans.factory.annotation.Qualifier
+import kotlin.annotation.AnnotationRetention.RUNTIME
+import kotlin.annotation.AnnotationTarget.FIELD
+import kotlin.annotation.AnnotationTarget.FUNCTION
+import kotlin.annotation.AnnotationTarget.TYPE_PARAMETER
+import kotlin.annotation.AnnotationTarget.VALUE_PARAMETER
+
+@Qualifier(value = "VideoEvents")
+@Retention(value = RUNTIME)
+@Target(FIELD, TYPE_PARAMETER, VALUE_PARAMETER, FUNCTION)
+annotation class VideoEvents
