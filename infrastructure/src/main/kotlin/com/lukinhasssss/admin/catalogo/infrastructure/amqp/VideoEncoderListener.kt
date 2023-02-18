724cc18a44b10555685eeb31c614cd37c22dc0c3
Lucas Monteiro <lukasmonteir0@hotmail.com>
2023-02-18 00:01:03 -0300

    feat: create amqp listener of video encoder

---
+++ infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/amqp/VideoEncoderListener.kt
@@ -0,0 +1,50 @@
+package com.lukinhasssss.admin.catalogo.infrastructure.amqp
+
+import com.lukinhasssss.admin.catalogo.application.video.media.update.UpdateMediaStatusCommand
+import com.lukinhasssss.admin.catalogo.application.video.media.update.UpdateMediaStatusUseCase
+import com.lukinhasssss.admin.catalogo.domain.video.MediaStatus.COMPLETED
+import com.lukinhasssss.admin.catalogo.infrastructure.configuration.json.Json
+import com.lukinhasssss.admin.catalogo.infrastructure.utils.log.Logger
+import com.lukinhasssss.admin.catalogo.infrastructure.video.models.VideoEncoderCompleted
+import com.lukinhasssss.admin.catalogo.infrastructure.video.models.VideoEncoderError
+import com.lukinhasssss.admin.catalogo.infrastructure.video.models.VideoEncoderResult
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.stereotype.Component
+
+@Component
+class VideoEncoderListener(
+    private val updateMediaStatusUseCase: UpdateMediaStatusUseCase
+) {
+
+    companion object {
+        const val LISTENER_ID = "videoEncodedListener"
+    }
+
+    @RabbitListener(id = LISTENER_ID, queues = ["\${amqp.queue.video-encoded.queue}"])
+    fun onVideoEncodedMessage(@Payload message: String) {
+        when (val aResult = Json.readValue(message, VideoEncoderResult::class.java)) {
+            is VideoEncoderCompleted -> {
+                Logger.info(
+                    message = "[message:video.listener.income] [status:completed] [payload:$message]",
+                    payload = message
+                )
+
+                val aCommand = UpdateMediaStatusCommand(
+                    videoId = aResult.id,
+                    status = COMPLETED,
+                    resourceId = aResult.video.resourceId,
+                    folder = aResult.video.encodedVideoFolder,
+                    filename = aResult.video.filePath
+                )
+
+                updateMediaStatusUseCase.execute(aCommand)
+            }
+
+            is VideoEncoderError -> Logger.error(
+                message = "[message:video.listener.income] [status:error] [payload:$message]",
+                payload = message
+            )
+        }
+    }
+}
