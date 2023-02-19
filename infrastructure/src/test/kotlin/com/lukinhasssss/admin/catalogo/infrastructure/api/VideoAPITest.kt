e1d89004a1c1040f6584ef4cefd7db69e71e243a
Lucas Monteiro <lukasmonteir0@hotmail.com>
2023-02-18 23:03:28 -0300

    test: add test for partial video creation endpoint implementation

--- infrastructure/src/test/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/api/VideoAPITest.kt
+++ infrastructure/src/test/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/api/VideoAPITest.kt
@@ -6,6 +6,7 @@ import com.lukinhasssss.admin.catalogo.application.video.create.CreateVideoOutpu
 import com.lukinhasssss.admin.catalogo.application.video.create.CreateVideoUseCase
 import com.lukinhasssss.admin.catalogo.domain.Fixture
 import com.lukinhasssss.admin.catalogo.domain.video.VideoID
+import com.lukinhasssss.admin.catalogo.infrastructure.video.models.CreateVideoRequest
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import io.mockk.verify
@@ -16,8 +17,10 @@ import org.springframework.http.MediaType
 import org.springframework.mock.web.MockMultipartFile
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.multipart
+import org.springframework.test.web.servlet.post
 import java.time.Year
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 @ControllerTest(controllers = [VideoAPI::class])
 class VideoAPITest {
@@ -120,6 +123,82 @@ class VideoAPITest {
                     assertEquals(expectedBanner.originalFilename, it.banner?.name)
                     assertEquals(expectedThumb.originalFilename, it.thumbnail?.name)
                     assertEquals(expectedThumbHalf.originalFilename, it.thumbnailHalf?.name)
+                }
+            )
+        }
+    }
+
+    @Test
+    fun givenAValidCommand_whenCallsCreatePartial_shouldReturnAnId() {
+        // given
+        val anime = Fixture.Categories.animes()
+        val shonen = Fixture.Genres.shonen()
+        val luffy = Fixture.CastMembers.luffy()
+
+        val expectedId = VideoID.unique()
+        val expectedTitle = Fixture.title()
+        val expectedDescription = Fixture.Videos.description()
+        val expectedLaunchYear = Year.of(Fixture.year())
+        val expectedDuration = Fixture.duration()
+        val expectedOpened = Fixture.bool()
+        val expectedPublished = Fixture.bool()
+        val expectedRating = Fixture.Videos.rating()
+        val expectedCategories = setOf(anime.id.value)
+        val expectedGenres = setOf(shonen.id.value)
+        val expectedMembers = setOf(luffy.id.value)
+
+        val aRequest = CreateVideoRequest(
+            title = expectedTitle,
+            description = expectedDescription,
+            launchYear = expectedLaunchYear.value,
+            duration = expectedDuration,
+            opened = expectedOpened,
+            published = expectedPublished,
+            rating = expectedRating.description,
+            categories = expectedCategories,
+            genres = expectedGenres,
+            members = expectedMembers
+        )
+
+        every { createVideoUseCase.execute(any()) } returns CreateVideoOutput(expectedId.value)
+
+        // when
+        val aResponse = mvc.post(urlTemplate = "/videos") {
+            accept = MediaType.APPLICATION_JSON
+            contentType = MediaType.APPLICATION_JSON
+            content = mapper.writeValueAsString(aRequest)
+        }.andDo { print() }
+
+        // then
+        aResponse.andExpect {
+            status { isCreated() }
+
+            header {
+                string(name = "Location", value = "/videos/${expectedId.value}")
+                string(name = "Content-Type", value = MediaType.APPLICATION_JSON_VALUE)
+            }
+
+            jsonPath("$.id", equalTo(expectedId.value))
+        }
+
+        verify {
+            createVideoUseCase.execute(
+                withArg {
+                    assertEquals(expectedTitle, it.title)
+                    assertEquals(expectedDescription, it.description)
+                    assertEquals(expectedLaunchYear.value, it.launchedAt)
+                    assertEquals(expectedDuration, it.duration)
+                    assertEquals(expectedOpened, it.opened)
+                    assertEquals(expectedPublished, it.published)
+                    assertEquals(expectedRating.description, it.rating)
+                    assertEquals(expectedCategories, it.categories)
+                    assertEquals(expectedGenres, it.genres)
+                    assertEquals(expectedMembers, it.members)
+                    assertNull(it.video)
+                    assertNull(it.trailer)
+                    assertNull(it.banner)
+                    assertNull(it.thumbnail)
+                    assertNull(it.thumbnailHalf)
                 }
             )
         }
