9135a7260e00bea67f08d82c6a126e6989d63dc9
Lucas Monteiro <lukasmonteir0@hotmail.com>
2023-04-10 22:11:15 -0300

    build: update rabbit for run in docker container

--- infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/configuration/AmqpConfig.kt
+++ infrastructure/src/main/kotlin/com/lukinhasssss/admin/catalogo/infrastructure/configuration/AmqpConfig.kt
@@ -8,11 +8,13 @@ import org.springframework.amqp.core.Binding
 import org.springframework.amqp.core.BindingBuilder
 import org.springframework.amqp.core.DirectExchange
 import org.springframework.amqp.core.Queue
+import org.springframework.amqp.rabbit.annotation.EnableRabbit
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@EnableRabbit
 class AmqpConfig {
 
     @Bean
