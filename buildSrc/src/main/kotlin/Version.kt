ced502bbeabd1328b904f8d0a29e9615c935cee6
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-01-28 15:44:12 -0300

    build: update project to JAVA 21 and gradle to 8.5

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,14 +1,14 @@
 object Version {
     const val PROJECT = "1.0.0"
-    const val KOTLIN = "1.8.21"
-    const val DETEKT = "1.23.0-RC3"
+    const val KOTLIN = "1.9.21"
+    const val DETEKT = "1.23.4"
     const val KTLINT = "11.3.1"
     const val JACOCO = "0.8.10"
     const val FLYWAY = "9.18.0"
     const val SONARQUBE = "4.0.0.2929"
-    const val SPRING_BOOT = "3.1.0"
+    const val SPRING_BOOT = "3.2.1"
     const val SPRING_DEPENDENCY_MANAGEMENT = "1.1.0"
-    const val TEST_CONTAINERS = "1.18.1"
+    const val TEST_CONTAINERS = "1.19.3"
     const val VAVR = "0.10.2"
     const val GATLING = "3.9.3.1"
 }
