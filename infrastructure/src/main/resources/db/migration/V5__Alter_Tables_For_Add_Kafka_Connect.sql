b49c8e3b2d4e7d1265ef1b025786b67b15e66a32
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-03-08 21:59:13 -0300

    refactor: add host for keycloak

--- infrastructure/src/main/resources/db/migration/V5__Alter_Tables_For_Add_Kafka_Connect.sql
+++ infrastructure/src/main/resources/db/migration/V5__Alter_Tables_For_Add_Kafka_Connect.sql
@@ -1,13 +1,13 @@
---alter table categories replica IDENTITY FULL;
---
---alter table genres replica IDENTITY FULL;
---alter table genres_categories replica IDENTITY FULL;
---
---alter table cast_members replica IDENTITY FULL;
---
---alter table videos_video_media replica IDENTITY FULL;
---alter table videos_image_media replica IDENTITY FULL;
---alter table videos replica IDENTITY FULL;
---alter table videos_categories replica IDENTITY FULL;
---alter table videos_genres replica IDENTITY FULL;
---alter table videos_cast_members replica IDENTITY FULL;
+alter table categories replica IDENTITY FULL;
+
+alter table genres replica IDENTITY FULL;
+alter table genres_categories replica IDENTITY FULL;
+
+alter table cast_members replica IDENTITY FULL;
+
+alter table videos_video_media replica IDENTITY FULL;
+alter table videos_image_media replica IDENTITY FULL;
+alter table videos replica IDENTITY FULL;
+alter table videos_categories replica IDENTITY FULL;
+alter table videos_genres replica IDENTITY FULL;
+alter table videos_cast_members replica IDENTITY FULL;
